bca21d663c9bb0bf134428722b55979f2b57ed2e
jpanimboza <61572277+jpanimboza@users.noreply.github.com>
2024-02-17 02:13:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,5 +10,15 @@ function onEachFeature(feature, layer) {
 // by clustering the markers contained within
 let markers = L.markerClusterGroup();
 
-var geojsonLayer = new L.GeoJSON.AJAX("map_airport.geojson");
-geojsonLayer.addTo(map);
+var counties = $.ajax({
+          url:"https://gist.githubusercontent.com/maptastik/df8e483d5ac1c6cae3dc4a7c02ea9039/raw/9cd46849bddcfa90aab240772a12275408d6d8d0/kyCounties.geojson",
+          dataType: "json",
+          success: console.log("County data successfully loaded."),
+          error: function (xhr) {
+            alert(xhr.statusText)
+          }
+        })
+$.when(counties).done(function() {
+            var map = L.map('map')
+                .setView([37.857507, -85.632935], 7);
+var kyCounties = L.geoJSON(counties.responseJSON).addTo(map);
