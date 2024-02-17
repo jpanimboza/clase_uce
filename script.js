582e724dbb06f575ec9d47bc093315ce30ef6159
jpanimboza <61572277+jpanimboza@users.noreply.github.com>
2024-02-17 02:26:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-var counties = $.ajax({
-            url: "https://gist.githubusercontent.com/maptastik/df8e483d5ac1c6cae3dc4a7c02ea9039/raw/9cd46849bddcfa90aab240772a12275408d6d8d0/kyCounties.geojson",
+var airports = $.ajax({
+            url: "map_airport.geojson",
             dataType: "json",
-            success: console.log("County data successfully loaded."),
-            error: function(xhr) {
-                alert(xhr.statusText)
-            }
         })
-$.when(counties).done(function() {
+$.when(airports).done(function() {
             var map = L.map('map')
                 .setView([37.857507, -85.632935], 7);
 
@@ -16,5 +12,5 @@ $.when(counties).done(function() {
                 maxZoom: 19
             }).addTo(map);
             // Add requested external GeoJSON to map
-            var kyCounties = L.geoJSON(counties.responseJSON).addTo(map);
+            var kyCounties = L.geoJSON(airports.responseJSON).addTo(map);
         });
