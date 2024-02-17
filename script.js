832ba01a5791805fee967afac69c5097580b2b63
jpanimboza <61572277+jpanimboza@users.noreply.github.com>
2024-02-17 00:27:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ let map = L.map('map').setView([-0.22,-78.51],10);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-fetch("map_airport.geojson").then(res => res.json()).then(data => {
-    // add GeoJSON layer to the map once the file is loaded
-    L.geoJson(data,{
-      pointToLayer: function(feature,layer){
-        var marker = L.marker(layer);
-        marker.bindPopup('<b>AIRPORT NAME : </b>'+feature.properties.name+' ('+feature.properties.abbrev+')<br>\
-        <b>TYPE OF AIPORT : </b>'+feature.properties.type+'<br>\
-        <a href="'+feature.properties.wikipedia+'">Wikipedia</a>').addTo(map);
-        marker.addTo(map)
-        return marker;
-      }
-    }
+// adding geojson by fetch
+// of course you can use jquery, axios etc.
+fetch("map_airport.geojson")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    // use geoJSON
+    L.geoJSON(data, {
+      onEachFeature: onEachFeature,
+    }).addTo(map);
+  });
