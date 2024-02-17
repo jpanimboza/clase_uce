17cce5d480637ec37890014c9260ee74d35fc339
jpanimboza <61572277+jpanimboza@users.noreply.github.com>
2024-02-17 02:20:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let map = L.map('map').setView([-0.22,-78.51],10);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-function onEachFeature(feature, layer) {
-  layer.bindPopup(feature.properties.name);
-}
-
 var counties = $.ajax({
-          url:"https://gist.githubusercontent.com/maptastik/df8e483d5ac1c6cae3dc4a7c02ea9039/raw/9cd46849bddcfa90aab240772a12275408d6d8d0/kyCounties.geojson",
-          dataType: "json",
-          success: console.log("County data successfully loaded."),
-          error: function (xhr) {
-            alert(xhr.statusText)
-          }
+            url: "https://gist.githubusercontent.com/maptastik/df8e483d5ac1c6cae3dc4a7c02ea9039/raw/9cd46849bddcfa90aab240772a12275408d6d8d0/kyCounties.geojson",
+            dataType: "json",
+            success: console.log("County data successfully loaded."),
+            error: function(xhr) {
+                alert(xhr.statusText)
+            }
         })
 $.when(counties).done(function() {
             var map = L.map('map')
                 .setView([37.857507, -85.632935], 7);
-var kyCounties = L.geoJSON(counties.responseJSON).addTo(map);
+
+            var basemap = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+                subdomains: 'abcd',
+                maxZoom: 19
+            }).addTo(map);
+            // Add requested external GeoJSON to map
+            var kyCounties = L.geoJSON(counties.responseJSON).addTo(map);
+        });
