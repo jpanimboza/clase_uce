44c4e3fe0c74b734f5a90b7edeefd0d82c96e613
jpanimboza <61572277+jpanimboza@users.noreply.github.com>
2024-02-17 00:56:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ let map = L.map('map').setView([-0.22,-78.51],10);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-// adding geojson by fetch
-// of course you can use jquery, axios etc.
-
 function onEachFeature(feature, layer) {
   layer.bindPopup(feature.properties.nazwa);
 }
 
-fetch("map_airport.geojson")
+// adding geojson by fetch
+// of course you can use jquery, axios etc.
+fetch("../static/wojewodztwa-medium.geojson")
   .then(function (response) {
     return response.json();
   })
